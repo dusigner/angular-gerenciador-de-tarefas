260d275b65bf40c4ea9fc99b4771f234f19bbdb2
Eduardo Soares <eduardo@dusigner.com.br>
2018-11-19 20:43:18 -0200

    finalized project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {  TarefaRoutes } from './tarefas';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tarefas/listar',
+    pathMatch: 'full'
+  },
+  ...TarefaRoutes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
